63474664ae6f70d23141819e5df5b93e1848b935
Alexander Williams <williams_alex@protonmail.com>
2019-10-04 16:15:47 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
+Math.sqrt
 def addition(num1, num2)
-
+  return num1+num2
 end
 
 def subtraction(num1, num2)
-
+  return num1-num2
 end
 
 def division(num1, num2)
-
+  return num1/num2
 end
 
 def multiplication(num1, num2)
-
+  return num1*num2
 end
 
 def modulo(num1, num2)
-
+  return num1%num2
 end
 
 def square_root(num)
-
+  return sqrt(num)
 end
